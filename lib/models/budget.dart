50a00cc2e20ae5fb9be6199f5111dd06189717ed
iamprasidh <iamprasidh@gmail.com>
2025-12-13 20:53:43 +0530

    implemented date in budget model

--- lib/models/budget.dart
+++ lib/models/budget.dart
@@ -1,11 +1,15 @@
 class Budget {
   final String id;
-  final String categoryId;
-  final double limit;
-  
+  final String categoryId; 
+  final double limit;      
+  final DateTime? startDate; 
+  final DateTime? endDate;   
+
   Budget({
-    required this.id, 
-    required this.categoryId, 
-    required this.limit
-    });
+    required this.id,
+    required this.categoryId,
+    required this.limit,
+    this.startDate,
+    this.endDate,
+  });
 }
