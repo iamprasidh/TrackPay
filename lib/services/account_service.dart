ea788c551b8fef9a02d472e248881021a9b8b6d6
iamprasidh <iamprasidh@gmail.com>
2025-12-13 22:39:06 +0530

    implemented service for account model

---
+++ lib/services/account_service.dart
@@ -0,0 +1,26 @@
+import 'package:hive/hive.dart';
+import '../models/account.dart';
+
+class AccountServices{
+  static const String boxName = "accounts";
+
+  static Future<Box<Account>> openBox() async {
+    return await Hive.openBox<Account>(boxName);
+  }
+
+  static Future<void> addAccount(Account account) async {
+    final box = await openBox();
+    await box.put(account.id, account);
+  }
+
+  static Future<void> updateAccount(Account account) async {
+    final box = await openBox();
+    await box.put(account.id, account);
+  }
+
+  static Future<void> deleteAccount(String id) async {
+    final box = await openBox();
+    await box.delete(id);
+  }
+}
+
