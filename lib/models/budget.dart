8c5c3ccea5985e3e70f113366886835a7584aff8
iamprasidh <iamprasidh@gmail.com>
2025-12-13 20:51:32 +0530

    created budget model

---
+++ lib/models/budget.dart
@@ -0,0 +1,11 @@
+class Budget {
+  final String id;
+  final String categoryId;
+  final double limit;
+  
+  Budget({
+    required this.id, 
+    required this.categoryId, 
+    required this.limit
+    });
+}
