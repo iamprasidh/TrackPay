fbd64ab242a8e6985017f499be34a4a426e34eda
iamprasidh <iamprasidh@gmail.com>
2025-12-13 21:27:45 +0530

    Registering Adapters for Account Models

--- lib/models/account.dart
+++ lib/models/account.dart
@@ -1,6 +1,13 @@
-class Account {
+import 'package:hive/hive.dart';
+part 'account.g.dart'; 
+
+@HiveType(typeId: 0)
+class Account extends HiveObject {
+  @HiveField(0)
   final String id;
+  @HiveField(1)
   final String accountName;
+  @HiveField(2)
   final double openingBalance;
 
   Account({
