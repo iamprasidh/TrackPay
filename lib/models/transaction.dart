2f36d43cdf645043bfe6ad9acfa52f51aa69154e
iamprasidh <iamprasidh@gmail.com>
2025-12-14 16:12:03 +0530

    implemented hive in dart

--- lib/models/transaction.dart
+++ lib/models/transaction.dart
@@ -1,12 +1,23 @@
+import 'package:hive/hive.dart';
+part 'transaction.g.dart';
+
 enum TransactionType { income, expense }
 
+@HiveType(typeId: 0)
 class Transaction {
+  @HiveField(0)
   final String id;
+  @HiveField(1)
   final DateTime date;
-  final String accountId;    
+  @HiveField(2)
+  final String accountId;
+  @HiveField(3)    
   final String categoryId;
+  @HiveField(4)
   final TransactionType transactionType;
+  @HiveField(5)
   final double amount;
+  @HiveField(6)
   final String? note;
 
   Transaction({
