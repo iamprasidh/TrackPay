0be0048f2c156930c1c17368885ecb98077fc4ad
iamprasidh <iamprasidh@gmail.com>
2025-12-14 16:43:41 +0530

    category services errors fixed

--- lib/services/category_services.dart
+++ lib/services/category_services.dart
@@ -1,19 +1,19 @@
 import 'package:hive/hive.dart';
-import '../models/account.dart';
+import '../models/category.dart';
 
 class CategoryServices{
   static const String boxName = "categories";
 
-  static Future<Box<Account>> openBox() async {
-    return await Hive.openBox<Account>(boxName);
+  static Future<Box<Category>> openBox() async {
+    return await Hive.openBox<Category>(boxName);
   }
 
-  static Future<void> addCategory(Account category) async {
+  static Future<void> addCategory(Category category) async {
     final box = await openBox();
     await box.put(category.id, category);
   }
 
-  static Future<void> updateCategory(Account category) async {
+  static Future<void> updateCategory(Category category) async {
     final box = await openBox();
     await box.put(category.id, category);
   }
