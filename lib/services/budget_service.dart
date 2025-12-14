9df9808d5421444385c6a76f374df3b9630c8e7e
iamprasidh <iamprasidh@gmail.com>
2025-12-14 17:11:01 +0530

    implemented getbudgets and fixed naming inconsistencies

--- lib/services/budget_service.dart
+++ lib/services/budget_service.dart
@@ -1,25 +1,31 @@
 import 'package:hive/hive.dart';
 import '../models/budget.dart';
 
-class BudgetServices {
+class BudgetService {
   static const String boxName = "budgets";
 
-  static Future<Box<Budget>> openbox() async {
+  static Future<Box<Budget>> openBox() async {
     return await Hive.openBox<Budget>(boxName);
   }
 
+static Future<List<Budget>> getBudgets() async {
+  final box = await openBox();
+  return box.values.toList();
+}
+
+
   static Future<void> addBudget(Budget budget) async {
-    final box = await openbox();
+    final box = await openBox();
     await box.put(budget.id, budget);
   }
 
   static Future<void> updateBudget(Budget budget) async {
-    final box = await openbox();
+    final box = await openBox();
     await box.put(budget.id, budget);
   }
 
   static Future<void> deleteBudget(String id) async {
-    final box = await openbox();
+    final box = await openBox();
     await box.delete(id);
   }
 }
