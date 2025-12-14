58a466236ccdbc4d9376c5fc6194c526084117a8
iamprasidh <iamprasidh@gmail.com>
2025-12-14 16:03:39 +0530

    implemented hive annotations for budget model

--- lib/models/budget.dart
+++ lib/models/budget.dart
@@ -1,8 +1,17 @@
+import 'package:hive/hive.dart';
+part 'budget.g.dart';
+
+@HiveType(typeId: 1)
 class Budget {
+  @HiveField(0)
   final String id;
+  @HiveField(1)
   final String categoryId; 
+  @HiveField(2)
   final double limit;      
+  @HiveField(3)
   final DateTime? startDate; 
+  @HiveField(4)
   final DateTime? endDate;   
 
   Budget({
