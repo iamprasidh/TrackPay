9d246c1234eb8a4ea2ade16e7d0151438360aa4f
iamprasidh <iamprasidh@gmail.com>
2025-12-14 16:11:48 +0530

    implemented hive in settings

--- lib/models/settings.dart
+++ lib/models/settings.dart
@@ -1,5 +1,11 @@
+import 'package:hive/hive.dart';
+part 'settings.g.dart';
+
+@HiveType(typeId: 3)
 class Settings{
+  @HiveField(0)
   bool isDarkMode;
+  @HiveField(1)
   String currency;
 
   Settings({
