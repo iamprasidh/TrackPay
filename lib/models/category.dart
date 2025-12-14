f83d55a85358620fe2edae9a1ce9e1ce559fde60
iamprasidh <iamprasidh@gmail.com>
2025-12-14 16:06:39 +0530

    implemented hive in category model

--- lib/models/category.dart
+++ lib/models/category.dart
@@ -1,6 +1,13 @@
+import 'package:hive/hive.dart';
+part 'category.g.dart';
+
+@HiveType(typeId: 2)
 class Category {
+  @HiveField(0)
   final String id;
+  @HiveField(1)
   final String categoryName;
+  @HiveField(2)
   final String? subCategoryName;
 
   Category({
