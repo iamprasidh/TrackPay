c84d190dd705bf610ea18a2042ff2cabe1a0936a
iamprasidh <iamprasidh@gmail.com>
2025-12-15 20:20:50 +0530

    implemented Notifier for Account

---
+++ lib/providers/account_notifier.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
+import '../models/account.dart';
+import '../services/account_service.dart';
+
+class AccountNotifier extends StateNotifier<List<Account>>{
+  AccountNotifier() :super([]) {
+    loadAccounts();
+  }
+
+  Future<void> loadAccounts() async {
+    final account = await AccountServices.getAccounts();
+    state = account;
+  }
+
+  Future<void> loadAccounts(Account account) async {
+    await AccountServices.addAccount(account);
+    await loadAccounts();
+  }
+
+  Future <void> deleteAccount(String id) async {
+    await AccountService.deleteAccount(id);
+    await loadAccounts();
+  }
+
+}
