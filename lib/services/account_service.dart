f5999bc8cb0496bfaa04b1afabd3d322e469db7b
iamprasidh <iamprasidh@gmail.com>
2025-12-15 22:03:29 +0530

    fixed getAccounts methot error

--- lib/services/account_service.dart
+++ lib/services/account_service.dart
@@ -4,6 +4,11 @@ import '../models/account.dart';
 class AccountService{
   static const String boxName = "accounts";
 
+    static Future<List<Account>> getAccounts() async {
+    final box = await Hive.openBox<Account>(boxName);
+    return box.values.toList();
+  }
+
   static Future<Box<Account>> openBox() async {
     return await Hive.openBox<Account>(boxName);
   }
