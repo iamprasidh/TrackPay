61116c14aa9590c3f84ba05701d4f0ac395dfa02
iamprasidh <iamprasidh@gmail.com>
2025-12-15 20:49:59 +0530

    implemented notifier for transaction

---
+++ lib/providers/transaction_notifier.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
+import 'package:trackpay/services/transaction_service.dart';
+import '../models/transaction.dart';
+import '../services/account_service.dart';
+
+class TransactionNotifier extends StateNotifier<List<Transaction>>{
+  TransactionNotifier() : super([]) {
+    loadTransaction();
+  }
+
+  Future<void> loadTransaction() async {
+    final transactions = await TransactionService.getTransactions();
+    state = transactions;
+  }
+
+   Future<void> addTransaction(Transaction transaction) async {
+    await TransactionService.addTransaction(transaction); 
+    await loadTransaction();
+  }
+
+    Future<void> deleteTransaction(string id) async {
+    await TransactionService.deletetransaction(id); 
+    await loadTransaction();
+  }
+}
