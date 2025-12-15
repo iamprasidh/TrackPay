a0fd20d9d018e229bcd1c5f85769731da261440e
iamprasidh <iamprasidh@gmail.com>
2025-12-15 21:06:35 +0530

    Implemented notifier for category

---
+++ lib/providers/category_notifier.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../models/category.dart';
+import '../services/category_service.dart';
+
+class CategoryNotifier extends StateNotifier<List<Category>> {
+  CategoryNotifier() : super([]) {
+    loadCategories();
+  }
+
+  Future<void> loadCategories() async {
+    final categories = await CategoryService.getCategories();
+    state = categories;
+  }
+
+  Future<void> addCategory(Category category) async {
+    await CategoryService.addCategory(category);
+    await loadCategories();
+  }
+
+  Future<void> deleteCategory(String id) async {
+    await CategoryService.deleteCategory(id);
+    await loadCategories();
+  }
+}
