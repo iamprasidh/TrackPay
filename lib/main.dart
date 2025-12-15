296f29433ac698769cf15ccb4e59dba182714908
iamprasidh <iamprasidh@gmail.com>
2025-12-15 21:31:04 +0530

    fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart'
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized()
+  WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
   
   runApp(
@@ -11,4 +11,20 @@ void main() async {
       child: MyApp(),
     ),
   );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'TrackPay',
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
 }
