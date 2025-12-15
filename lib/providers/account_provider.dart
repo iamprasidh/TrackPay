02eb02d7bb6d9471d3d3ba80f217f7227e66a989
iamprasidh <iamprasidh@gmail.com>
2025-12-15 20:21:25 +0530

    implemented provider for notifier

--- lib/providers/account_provider.dart
+++ lib/providers/account_provider.dart
@@ -1,7 +1,8 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../models/account.dart';
-import '../services/account_service.dart';
+import 'account_notifier.dart';
 
-final accountProvider = FutureProvider<List<Account>>((ref) async {
-  return AccountService.getAccounts();
-});
+final accountNotifierProvider =
+    StateNotifierProvider<AccountNotifier, List<Account>>(
+  (ref) => AccountNotifier(),
+);
