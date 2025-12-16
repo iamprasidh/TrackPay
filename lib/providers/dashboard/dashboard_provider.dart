b52dd3dd07cec429febeb77d7aecf75292217ecc
iamprasidh <iamprasidh@gmail.com>
2025-12-16 22:10:46 +0530

    implemented transaction calculation logic

--- lib/providers/dashboard/dashboard_provider.dart
+++ lib/providers/dashboard/dashboard_provider.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../transaction_provider.dart';
 import '../../models/transaction.dart';
+import '../account_provider.dart';
 
 final totalIncomeProvider = Provider<double>((ref) {
   final transactions = ref.watch(transactionProvider);
@@ -19,10 +20,21 @@ final totalExpenseProvider = Provider<double>((ref) {
 });
 
 final balanceProvider = Provider<double>((ref) {
-  final income = ref.watch(totalIncomeProvider);
-  final expense = ref.watch(totalExpenseProvider);
+  final accounts = ref.watch(accountNotifierProvider); // all accounts
+  final transactions = ref.watch(transactionProvider);
 
-  return income - expense;
+  final income = transactions
+      .where((t) => t.transactionType == TransactionType.income)
+      .fold<double>(0.0, (sum, t) => sum + t.amount);
+
+  final expense = transactions
+      .where((t) => t.transactionType == TransactionType.expense)
+      .fold<double>(0.0, (sum, t) => sum + t.amount);
+
+  final totalAccountBalance = accounts.fold<double>(
+      0.0, (sum, a) => sum + a.openingBalance);
+
+  return totalAccountBalance + income - expense;
 });
 
 final recentTransactionsProvider = Provider<List<Transaction>>((ref) {
