e115de5360bbb21800d65e8cb9e8d10fd2cdffe2
iamprasidh <iamprasidh@gmail.com>
2025-12-17 19:57:02 +0530

    fixed username not fetching error

--- lib/providers/settings_provider.dart
+++ lib/providers/settings_provider.dart
@@ -1,8 +1,25 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive/hive.dart';
 import '../models/settings.dart';
 
 class SettingsNotifier extends StateNotifier<Settings> {
-  SettingsNotifier() : super(Settings.defaultSettings());
+  SettingsNotifier() : super(Settings.defaultSettings()) {
+    _loadSavedSettings();
+  }
+
+  Future<void> _loadSavedSettings() async {
+    try {
+      final box = await Hive.openBox('user');
+      final savedName = box.get('name');
+      
+      if (savedName != null && savedName is String && savedName.isNotEmpty) {
+        state = state.copyWith(userName: savedName);
+      }
+    } catch (e) {
+      // If there's an error loading saved settings, use defaults
+      print('Error loading saved settings: $e');
+    }
+  }
 
   void updateUserName(String name) {
     state = state.copyWith(userName: name);
